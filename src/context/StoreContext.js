4ca70a6d6ffd3edaba05873740349fff08f14ffe
Mehak Fatima <mehak.fatima2386@gmail.com>
2024-08-31 17:01:14 +0500

    updated

--- src/context/StoreContext.js
+++ src/context/StoreContext.js
@@ -18,18 +18,25 @@ const StoreContextProvider = (props) => {
     const removeFromCart = (itemId) => {
         setCart((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cart) {
+            if (cart[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount += itemInfo.price * cart[item];
+            }
+        }
+        return totalAmount;
+    }
     const contextValue =
     {
         food_list,
         cart,
         setCart,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount
     };
-
-    useEffect(() => {
-        console.log(cart)
-    }, [cart])
 
     return (
         <StoreContext.Provider value={contextValue}>
